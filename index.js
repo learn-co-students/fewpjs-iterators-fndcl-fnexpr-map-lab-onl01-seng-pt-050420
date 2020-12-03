13d3d8c17ba9252ac748c8b2de6bc60b43dde2e0
James Rogers <jrogers.dev@gmail.com>
2020-12-03 22:04:24 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(toTitleCase);
+}
+
+function toTitleCase(str) {
+  return str.replace(/\b[a-zA-Z]/g, (match) => match.toUpperCase()
+    );
 }
